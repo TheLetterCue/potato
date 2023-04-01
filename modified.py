dc899589f6c01c8e776c82c59fe727c47f3312a9
Thomas Jones <howell.tomjones@gmail.com>
2023-04-01 22:43:15 +0100

    Added a modified rolling algorithm to test if it was equivalent, it was not

---
+++ modified.py
@@ -0,0 +1,45 @@
+from random import randint
+
+def Roll():
+    match randint(1,6):
+        case 1|2:
+            match randint(1,6):
+                case 1:
+                    return [0,1,0,0]
+                case 2|3|4|5|6:
+                    return [1,0,0,0]
+        case 3|4:
+            return [0,0,1,0]
+        case 5|6:
+            return [0,0,0,1]
+
+def Play(use_potatoes = False, verbose = False):
+    stats = [0,0,0,1]
+    while max(stats[:-1]) < 10:
+        while use_potatoes and stats[1] >= stats[3]:
+            stats[1] -= stats[3]
+            stats[2] -= 1
+        stats = [sum(a) for a in zip(stats, Roll())]
+    
+    match (result := stats[:-1].index(max(stats[:-1]))):
+        case 0:
+            if verbose:print("Wizard kidnapping")
+        case 1:
+            if verbose:print("Cozy hole")
+        case 2:
+            if verbose:print("Eaten by orcs")
+    return result
+
+EXPERIMENTS = 10000
+
+wins = 0
+for a in range(EXPERIMENTS):
+    if Play(use_potatoes=True) != 2:
+        wins += 1
+print(f"{wins/EXPERIMENTS} wins per experiment")
+
+wins = 0
+for a in range(EXPERIMENTS):
+    if Play() != 2:
+        wins += 1    
+print(f"{wins/EXPERIMENTS} wins per experiment")
